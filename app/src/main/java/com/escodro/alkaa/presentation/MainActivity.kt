6b8f3161ab39572ada43a19d1b5484f0cd7d83ec
Igor Escodro <escodro@outlook.com>
2020-06-01 15:08:07 -0300

    :lipstick: Update Main Activity
    Main Activity updated to base fragment navigation

--- app/src/main/java/com/escodro/alkaa/presentation/MainActivity.kt
+++ app/src/main/java/com/escodro/alkaa/presentation/MainActivity.kt
@@ -2,18 +2,22 @@ package com.escodro.alkaa.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.escodro.alkaa.R
+import com.escodro.alkaa.databinding.ActivityMainBinding
 import timber.log.Timber
 
 /**
  * Main application [AppCompatActivity].
  */
-class MainActivity : AppCompatActivity() {
+internal class MainActivity : AppCompatActivity() {
+
+    @Suppress("LateinitUsage")
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Timber.d("onCreate")
 
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
