31b3eeed08b512142d1c06c344edebe0ec555ccf
Igor Escodro <escodro@outlook.com>
2020-06-08 09:27:53 -0300

    :lipstick: Implement basic navigation
    Basic navigation added

--- app/src/main/java/com/escodro/alkaa/presentation/MainActivity.kt
+++ app/src/main/java/com/escodro/alkaa/presentation/MainActivity.kt
@@ -2,6 +2,9 @@ package com.escodro.alkaa.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.escodro.alkaa.R
 import com.escodro.alkaa.databinding.ActivityMainBinding
 import timber.log.Timber
 
@@ -19,5 +22,18 @@ internal class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val host = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as? NavHostFragment?
+
+        if (host == null) {
+            Timber.e("NavHostFragment is null ")
+            return
+        }
+
+        NavigationUI.setupWithNavController(binding.navBottomView, host.navController)
     }
 }
